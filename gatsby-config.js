193603508b27f60f818d101d20abb33fe12754b3
Jacqueline Nguyen <beuyenj@gmail.com>
2020-11-21 18:55:42 -0800

    Added source branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteMetadata: {
+    siteMetadata: {
     title: 'Gatsby Starter - Dimension V2',
     author: 'Hunter Chang',
     description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
